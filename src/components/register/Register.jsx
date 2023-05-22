177c60cc8a919a3d8c74f87f96d961b43828700c
Emilix22 <torresdragon@hotmail.com>
2023-05-22 14:51:52 -0300

    envio post de register con formData

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -4,16 +4,16 @@ import Person2Icon from '@mui/icons-material/Person2';
 import {useHistory} from 'react-router-dom'
 
 function Register({ setNewRegister }) {
-    const formData = new FormData();
     const history = useHistory()
-
+    
     const [name, setName] = useState();
     const [surname, setSurname] = useState();
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
-    const [image, setImage] = useState();
+    const [image, setImage] = useState(null);
     const [errors, setErrors] = useState();
     
+    const formData = new FormData();
     formData.append('name', name)
     formData.append('surname', surname)
     formData.append('email', email)
@@ -22,14 +22,14 @@ function Register({ setNewRegister }) {
 
     const handleSubmit = (event) => {
         event.preventDefault()
-
+        
         fetch('http://localhost:3000/api/users/create', {
             method: 'POST',
-            body: formData
+            body: formData,
         })
         .then(res => res.json())
         .then(info => {
-            console.log(info)
+           console.log(info)
             {
                 if (info.error) {
                     setErrors(info.error)
@@ -47,9 +47,8 @@ function Register({ setNewRegister }) {
         <form className='register-container' >
             <h3 className='welcome-register'>BIENVENID@</h3>
                 <Person2Icon fontSize='large' />
-                {/* ver los errores que son de express validator cambiar los que tienen _ */}
                 {
-                    errors ? <span className='error-msg'> {errors} </span> : ''   
+                    errors && !(errors.name || errors.surname || errors.password || errors.email) ? <span className='error-msg'> {errors} </span> : ''   
                 }
             <div className='register-form'>
                 <div className='form-group'>
@@ -61,6 +60,10 @@ function Register({ setNewRegister }) {
                      placeholder='Ingrese su nombre...'
                      onChange={(e) => setName(e.target.value)}
                     />
+                    {
+                        errors && errors.name ? <span className='error-msg'> {errors.name.msg} </span> : ''
+                   
+                    }
                 </div>
                 <div className='form-group'>
                     <label htmlFor="surname">Apellido</label>
@@ -71,6 +74,10 @@ function Register({ setNewRegister }) {
                      placeholder='Ingrese su apellido...'
                      onChange={(e) => setSurname(e.target.value)}
                     />
+                    {
+                        errors && errors.surname ? <span className='error-msg'> {errors.surname.msg} </span> : ''
+                   
+                    }
                 </div>
                 <div className='form-group'>
                     <label htmlFor="email">Email</label>
@@ -81,6 +88,10 @@ function Register({ setNewRegister }) {
                      placeholder='Ingrese su Email...'
                      onChange={(e) => setEmail(e.target.value)}
                     />
+                    {
+                        errors && errors.email ? <span className='error-msg'> {errors.email.msg} </span> : ''
+                   
+                    }
                 </div>
                 <div className='form-group'>
                     <label htmlFor="password">Password</label>
@@ -89,7 +100,12 @@ function Register({ setNewRegister }) {
                      type="password"
                      name='password'
                      placeholder='Ingrese su Password...'
-                     onChange={(e) => setPassword(e.target.value)} />
+                     onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {
+                        errors && errors.password ? <span className='error-msg'> {errors.password.msg} </span> : ''
+                   
+                    }
                 </div>
                 <div className='form-group-image'>
                     <label htmlFor="image">Foto de perfil</label>
@@ -97,7 +113,8 @@ function Register({ setNewRegister }) {
                      className='form-input'
                      type="file"
                      name='image'
-                     onChange={(e) => setImage(e.target.files[0])} />
+                     onChange={(e) => setImage(e.target.files[0])} 
+                    />
                 </div>
                 
                 <button onClick={handleSubmit}>Registrarme</button>
